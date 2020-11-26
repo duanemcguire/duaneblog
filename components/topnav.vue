85388f0eec818ac5a2b1bdb1931d04aa646283b7
Duane McGuire <biz@duanemcguire.com>
2020-11-26 11:08:17 -0700

    subtract some mcpiano

--- components/topnav.vue
+++ components/topnav.vue
@@ -2,9 +2,6 @@
 <div class="topnav pt-3" id="myTopnav">
   <a href="/">Home</a>
   <a href="/blog">Blog</a>
-  <a href="/category/pianos-for-sale">Pianos For Sale</a>
-  <a href="/category/shop-work">Shop Work</a>
-  <a href="/contact">Contact</a>
   </ul>
 </div>
 </template>
