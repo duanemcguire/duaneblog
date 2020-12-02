8971206d297b6d6ad0869edbf6d17415bb13d44e
Duane McGuire <biz@duanemcguire.com>
2020-12-01 20:57:25 -0700

    disqus

--- components/disq-comments.vue
+++ components/disq-comments.vue
@@ -1,16 +1,6 @@
 <!-- ~/components/Comments.vue -->
 <template>
 <div>
-  <vue-disqus shortname="blogduanemcguirecom" :identifier="`duaneblog-${blog.slug}`" :url="`https://blog.duanemcguire.com/blog/${blog.slug}`"></vue-disqus>
+  <Disqus />
 </div>
 </template>
-<script>
-export default {
-  props: {
-    blog: {
-      type: Object,
-      required: true
-    }
-  }
-}
-</script>
