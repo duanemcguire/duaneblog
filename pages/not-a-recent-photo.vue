4208da80826b60a3dd83d63c0b032329864b0982
Duane McGuire <biz@duanemcguire.com>
2020-12-12 08:16:06 -0700

    not a recent photo

--- pages/not-a-recent-photo.vue
+++ pages/not-a-recent-photo.vue
@@ -6,6 +6,20 @@
     <BR />
     <img src="/images/not-a-recent-photo.jpg" />
     <BR />
+    <h2>Santa Claus Parade</h2>
+    <h3>(1965)</h3>
+    <a href="/images/santa-claus-parade.jpg"><img src="/images/santa-claus-parade.resized.jpg" /></a>
+    <BR />
+    This photo, from Tom Mattson's photo archive, is the Raymond, Washington Santa Claus parade of 1965.
+    I'm near the back, in the clarinet section. When this photo first appeared on Facebook, it was of particular interest to me because it captures my Dad, Harvey McGuire, among the spectators. He was a lover of music: a big fan of the band, high
+    school theater, and local musical productions.
+    <BR />
+    <img src="/images/harvey-in-the-crowd.png" />
+    <img src="/images/duane-in-the-band.png" />
+    <iframe :src="'/hyvor.html?/not-a-recent-photo'  " width="100%" height="1000">
+    </iframe>
+
+    <BR /><BR />
     And that's the way it was!
   </div>
 </div>
