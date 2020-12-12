792fefc9b9404b785c476b473f5ca7f514403932
Duane McGuire <biz@duanemcguire.com>
2020-12-12 09:03:17 -0700

    correct date

--- pages/not-a-recent-photo.vue
+++ pages/not-a-recent-photo.vue
@@ -2,15 +2,15 @@
 <div>
   <div class="container">
     <BR />
-    In 1965, Raymond High School Marching Band acquired new uniforms!
+    In 1966, Raymond High School Marching Band acquired new uniforms!
     <BR />
     <img src="/images/not-a-recent-photo.jpg" />
     <BR />
     <h2>Santa Claus Parade</h2>
-    <h3>(1965)</h3>
+    <h3>(1966)</h3>
     <a href="/images/santa-claus-parade.jpg"><img src="/images/santa-claus-parade.resized.jpg" /></a>
     <BR />
-    This photo, from Tom Mattson's photo archive, is the Raymond, Washington Santa Claus parade of 1965.
+    This photo, from Tom Mattson's photo archive, is the Raymond, Washington Santa Claus parade of 1966.
     I'm near the back, in the clarinet section. When this photo first appeared on Facebook, it was of particular interest to me because it captures my Dad, Harvey McGuire, among the spectators. He was a lover of music: a big fan of the band, high
     school theater, and local musical productions.
     <BR />
